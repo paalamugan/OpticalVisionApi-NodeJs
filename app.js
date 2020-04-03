f4aec87cbbbcafd87c7712af4a2c24db4aff3bc6
paalamugan <42642576+paalamugan@users.noreply.github.com>
2020-04-03 09:24:17 +0530

    Added node engine details

--- app.js
+++ app.js
@@ -22,13 +22,12 @@ app.use(bodyPraser.json());
 app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(function(req,res,next){
 
-  res.header('Access-Control-Allow-Origin',"*");
+  res.header('Access-Control-Allow-Origin',"http://localhost:4200");
   res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type, Accept, Authorization');
-  if(req.method === 'OPTIONS'){
-  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-  return res.status(200).json({});
-  }
-  
+  // if(req.method === 'OPTIONS'){
+  // res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  // return res.status(200).json({});
+  // }
   next();
   });
  app.use(session({
